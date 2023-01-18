6485b9eff303a651444b0350dfbc6f08b8d366b0
Dicse Gábor <47577199+dicsegabor@users.noreply.github.com>
2023-01-18 18:15:36 +0100

    initial upload

---
+++ lua/user/options.lua
@@ -0,0 +1,3 @@
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = true
+lvim.colorscheme = "lunar"
---
+++ lua/user/plugins.lua
@@ -0,0 +1,13 @@
+-- Additional Plugins
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+lvim.plugins = {
+  {
+    'preservim/tagbar',
+  }
+}
