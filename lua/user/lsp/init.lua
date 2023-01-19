043a04c87cc11adad161d8fe8ef800844a112297
Dicse Gábor <47577199+dicsegabor@users.noreply.github.com>
2023-01-19 16:32:36 +0100

    clangd setup somehow works

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,6 +3,14 @@ if not status_ok then
   return
 end
 
--- require "user.lsp.mason"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require "user.lsp.settings.clangd"
+
+-- local servers = {
+--   "clangd",
+-- }
+
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, servers)
+
+-- for server in servers do
+--   require("user.lsp.settings." .. server)
+-- end
