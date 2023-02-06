7601573abd5bf02d0c0c689cbc6a7f2f13b6ef76
Dicse Gábor <47577199+dicsegabor@users.noreply.github.com>
2023-02-06 17:27:55 +0100

    Added comment for clarity

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,6 +4,6 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["<F8>"] = ":TagbarOpenAutoClose<CR>"
+lvim.keys.normal_mode["<F8>"] = ":TagbarOpenAutoClose<CR>" -- For opening the tagbar, and closing automatically
 
 -- Insert mode
