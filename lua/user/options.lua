f79855cf0973d5e0779d5ae0c4793e496381bed0
Dicse Gábor <47577199+dicsegabor@users.noreply.github.com>
2023-11-15 21:15:33 +0100

    Virtualtext deprecation fixed

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,6 +6,6 @@ lvim.log.level = "warn"
 -- }
 
 lvim.colorscheme = "lunar"
-lvim.lsp.diagnostics.virtual_text = false
+vim.diagnostic.config({ virtual_text = false })
 lvim.format_on_save.enabled = true
 lvim.format_on_save.pattern = { "*.py", "*.json", "*.html", "*.css", "*.rs", "*.lua" }
